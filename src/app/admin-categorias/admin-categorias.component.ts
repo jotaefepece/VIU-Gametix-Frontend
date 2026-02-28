8af1c3c3b99c458f68dcff60a18bf8acdb81318f
hferrer08 <hubert.ferrerg8@gmail.com>
2026-02-28 08:49:03 -0300

    Creación de componentes pago, admin-productos y admin-categorias

---
+++ src/app/admin-categorias/admin-categorias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-categorias',
+  imports: [],
+  templateUrl: './admin-categorias.component.html',
+  styleUrl: './admin-categorias.component.css'
+})
+export class AdminCategoriasComponent {
+
+}
---
+++ src/app/admin-productos/admin-productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-productos',
+  imports: [],
+  templateUrl: './admin-productos.component.html',
+  styleUrl: './admin-productos.component.css'
+})
+export class AdminProductosComponent {
+
+}
